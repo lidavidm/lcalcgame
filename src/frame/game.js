f46843fdf0697a80dd7e5d70b9c6e5413320e307
David Li <li.davidm96@gmail.com>
2017-01-26 17:52:02 -0500

    Show env display if variables are in toolbox as well

--- src/frame/game.js
+++ src/frame/game.js
@@ -40,8 +40,9 @@ class Level {
 
         var canvas_screen = stage.boundingSize;
 
-        const showEnvironment = Object.keys(this.globals.bindings).length > 0 ||
-              mag.Stage.getNodesWithClass(VarExpr, [], true, this.exprs).length > 0;
+        const showEnvironment = Object.keys(this.globals.bindings).length > 0
+              || mag.Stage.getNodesWithClass(VarExpr, [], true, this.exprs).length > 0
+              || mag.Stage.getNodesWithClass(VarExpr, [], true, this.toolbox).length > 0;
         const envDisplayWidth = 0.20 * canvas_screen.w;
 
         GLOBAL_DEFAULT_SCREENSIZE = stage.boundingSize;
